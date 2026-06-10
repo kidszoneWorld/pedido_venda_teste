27ae6367cd114a4f8f81c9e27633745090e1f993
Luis Henrique <Luis.h@kidszone.local>
2026-06-10 10:26:26 -0300

    sobe acessos ao site pelo Postgree 5

--- config/database.js
+++ config/database.js
@@ -1,11 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.POST_USER,
-    host: process.env.POST_URL,
-    database: process.env.POST_DB,
-    password: process.env.POST_SENHA,
-    port: Number(process.env.POST_PORT),
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
