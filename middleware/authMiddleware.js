286f59338f822bb7ea3670880e48fcd13aaf5d53
Luis Henrique <Luis.h@kidszone.local>
2026-06-10 12:04:28 -0300

    sobe acessos ao site pelo Postgree teste6

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -17,51 +17,15 @@ async function authenticateUser(req, res) {
         const email = req.body?.email;
         const senha = req.body?.senha;
 
-        if (!email || !senha) {
-            return res.status(400).json({
-                error: 'Email ou senha ausentes'
-            });
-        }
-
-        const result = await pool.query(
-            `SELECT *
-             FROM "TbUsuarios"
-             WHERE "UsuEmail" = $1`,
-            [email]
-        );
-
-        console.log(result.rows);
-
-        if (result.rows.length === 0) {
-            return res.status(401).send('Usuário não encontrado');
-        }
-
-        const user = result.rows[0];
-
-        console.log('USER:', user);
-
-        // IMPORTANTE:
-        // PostgreSQL geralmente retorna lowercase
-        const senhaBanco = user.UsuSenha || user.ususenha;
-
-        if (senha !== senhaBanco) {
-            return res.status(401).send('Senha incorreta');
-        }
-
-        // req.session.isAuthenticated = true;
-
-        // req.session.user = {
-        //     id: user.UsuId || user.usuid,
-        //     email: user.UsuEmail || user.usuemail,
-        //     nome: user.UsuNome || user.usunome,
-        //     numero: user.UsuNumero || user.usunumero
-        // };
-        return res.send('login ok')
-        return res.redirect('/');
+        return res.json({
+            ok: true,
+            email,
+            senha
+        });
 
     } catch (error) {
 
-        console.error('ERRO AUTH:', error);
+        console.error('ERRO REAL:', error);
 
         return res.status(500).json({
             message: error.message,
