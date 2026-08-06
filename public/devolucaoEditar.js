99e559c30c724dc676d3f27ef1c03f7c70122719
Luis Henrique <Luis.h@kidszone.local>
2026-08-06 16:05:06 -0300

    add Editar devoluções pendentes muda header descomenta linhas

--- public/devolucaoEditar.js
+++ public/devolucaoEditar.js
@@ -384,7 +384,7 @@ function configurarEventos(){
 
                 }
 
-                adicionarNovaLinhaEditavel();
+                // adicionarNovaLinhaEditavel();
 
             }
         );
@@ -536,10 +536,10 @@ function verificarPermissaoEdicao(dev){
             'salvarEdicaoDevolucao'
         );
 
-    const btnAdicionar =
-        el(
-            'adicionarLinha'
-        );
+    // const btnAdicionar =
+    //     el(
+    //         'adicionarLinha'
+    //     );
 
 
 
@@ -1183,7 +1183,7 @@ function navegarEnterTab(indiceColuna, linhaAtual, linhas, tr){
 
         if(linhaAtual === linhas.length - 1){
 
-            adicionarNovaLinhaEditavel();
+            // adicionarNovaLinhaEditavel();
 
             setTimeout(
                 () => {
